9e0ea65f02a0b2002c3b9dce34206b7be952f6fa
DLekunovich <dlekunovic@vgsi.com>
2024-04-17 21:32:07 +0200

    Full project

--- src/main/java/org/example/app/Controller.java
+++ src/main/java/org/example/app/Controller.java
@@ -7,11 +7,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 
 public class Controller {
 
@@ -27,7 +24,7 @@ public class Controller {
         loadInfo();
     }
 
-    public void buttonAddClick(ActionEvent actionEvent){
+    public void buttonAddClick(){
         try{
             if(!product_field.getText().trim().isEmpty()) {
                 db.addProduct(product_field.getText());
